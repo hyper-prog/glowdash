2fdfd62f74a034b9ec68c166e0dd6fd40e817e14
Peter Deak <hyper80@gmail.com>
2024-11-25 22:46:30 +0100

    Increment static file version

--- glowdash/glowdash.go
+++ glowdash/glowdash.go
@@ -140,7 +140,7 @@ var CommSSEHost string = ""
 var CommSSEPort int = 8085
 var BackgroudDevQueryNetDialerTimeout time.Duration = time.Duration(1200) * time.Millisecond
 var BackgroudDevQueryNetKeepaliveTimeout time.Duration = time.Duration(1200) * time.Millisecond
-var AssetVer string = "109"
+var AssetVer string = "111"
 
 var Panels []PanelInterface
 var Pages []PageInterface
